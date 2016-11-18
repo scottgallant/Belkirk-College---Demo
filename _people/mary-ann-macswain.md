eea0be7dbb84d37c4df7d3a7ddbc9e652952a37c
Scott Gallant <scottgallant@gmail.com>
2016-11-18 10:57:17 -0500

    first commit

---
+++ _people/mary-ann-macswain.md
@@ -0,0 +1,18 @@
+---
+title: Mary-Ann MacSwain
+date: '2016-11-08 00:00:00'
+layout: person
+sub-title: Data Analyst
+thumbnail: forestryio/images/MacSwain%20Mary-Ann-1.jpg
+tags (staff, board, advisor, partner):
+- staff
+email: mmmacswain@upei.ca
+linkedin: ''
+---
+Mary-Ann MacSwain joined the CHCR in 2016 as a Data Analyst.
+
+Mary-Ann received her B.A. (Honours) in Psychology from UPEI, where she is currently completing her MSc in Epidemiology.
+
+Prior to joining the CHCR, Mary-Ann worked as a Research Officer for the Correctional Service of Canada’s Addictions Research Centre, a Health Information Specialist for Health PEI, and as a Data Analyst and Chronic Disease Epidemiologist within PEI’s Chief Public Health Office.
+
+She brings over ten years of experience working in research, and is responsible for statistical analysis, data management, research design and execution.
---
+++ js/main.js
@@ -0,0 +1,105 @@
+---
+---
+$(function() {
+	// fade in hero bg image
+	$(function(){
+	    var bgimage = new Image();
+	    bgimage.src="{{ site.baseurl }}/images/banner_upei.png"
+	    $(bgimage).load(function(){
+	        $("#banner").css("background-image","url("+$(this).attr("src")+")").css("opacity", "1");
+	     });
+	});
+
+	// hover intent on main nav
+	function slideDown(){
+		console.log("make tall");
+		$(this).children('ul').slideDown(200);
+	}
+	function slideUp(){
+		console.log("make tall");
+		$(this).children('ul').slideUp(200);
+	}
+	$("li.nested").hoverIntent({
+		over: slideDown,
+    out: slideUp,
+    timeout: 300
+   });
+
+	 // prevent the top-level about link from navigating (opens dropdown on hover)
+	$('.nested > a[href="/chbmr/about"], .nested > a[href="/about"]').on('click', function(e){
+		e.preventDefault();
+	});
+
+	// make cards clickable (in grid layout)
+  $(".card").on('click', function(){
+    window.location = $(this).find('a').attr('href');
+  });
+
+	// make people clickable (in grid layout)
+  $(".column-list-layout .post").on('click', function(){
+    window.location = $(this).find('a').attr('href');
+  });
+
+// flex slider for homepage logos
+// store the slider in a local variable
+  var $window = $(window),
+      flexslider = { vars:{} };
+
+  // tiny helper function to add breakpoints
+  function getGridSize() {
+    return (window.innerWidth < 600) ? 2 :
+           (window.innerWidth < 900) ? 3 : 4;
+  }
+
+	// search on focus
+	$("input.search").on('focus', function(){
+		$(this).siblings('.icon-search').addClass('focus');
+	});
+	$("input.search").on('blur', function(){
+		$(this).siblings('.icon-search').removeClass('focus');
+	});
+
+	// flexslider
+  $window.load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      animationLoop: true,
+      itemWidth: 70,
+      itemMargin: 120,
+			directionNav: false,
+      minItems: getGridSize(), // use function to pull in initial value
+      maxItems: 3 // use function to pull in initial value
+    });
+  });
+
+  // check grid size on resize event
+  $window.resize(function() {
+    var gridSize = getGridSize();
+
+    flexslider.vars.minItems = gridSize;
+    flexslider.vars.maxItems = gridSize;
+  });
+
+	// make table rows clickable
+  $(".data-table tbody tr").on('click', function(){
+    window.location = $(this).find('a').attr('href');
+  });
+
+  $('#publications').DataTable({
+    lengthChange: false,
+    info: false,
+    language: {
+      searchPlaceholder: "Search",
+      search: "",
+      paginate: {
+        previous: '<i class="fa fa-chevron-left"></i>',
+        next:     '<i class="fa fa-chevron-right"></i>'
+      }
+    }
+  });
+
+  $('.twitter-wrap').on('DOMSubtreeModified propertychange',"#twitter-widget-0", function() {
+    $(".twitter-timeline").contents().find(".timeline-Tweet-media").css("display", "none");
+    $(".twitter-block").css("height", "100%");
+  });
+});
